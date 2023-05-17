6bb81de7815450974a353cc0bd8a6084d4407fdf
Gaizka Basterra <66625499+gaizka-basterra@users.noreply.github.com>
2023-05-17 17:39:19 +0200

    Update bin_BCD.vhdl

--- bin_BCD.vhdl
+++ bin_BCD.vhdl
@@ -7,83 +7,113 @@ entity bin_BCD is
 port(
 clk: in std_logic;
 inicio: in std_logic;
-sw: in std_logic_vector (3 downto 0);
+sw: in std_logic_vector (11 downto 0);
+tipo: in std_logic_vector (1 downto 0);
 enable: in std_logic;
-fin: out std_logic;
-unidades: out std_logic_vector (3 downto 0);
-decenas: out std_logic_vector (3 downto 0)
+enable_seg: out std_logic_vector (3 downto 0);
+segmentos: out std_logic_vector (6 downto 0)
 );
 end bin_BCD;
 
 architecture Behavioral of bin_BCD is
 
-signal vector_aux: std_logic_vector (11 downto 0);
-signal estado: std_logic_vector (1 downto 0);
-signal cont_bits: integer range 0 to 3;
+signal num_aux: integer range 0 to 5000;
+signal num_aux1: integer range 0 to 99;
+signal num_aux2: integer range 0 to 99;
 
-signal cont: integer range 0 to 100000000;
+signal sal_mux: integer range 0 to 9;
+signal unidades: integer range 0 to 9;
+signal decenas: integer range 0 to 9;
+signal centenas: integer range 0 to 9;
+signal millares: integer range 0 to 9;
+signal enable_aux: std_logic_vector (3 downto 0);
+signal cont_base_enable: integer range 0 to 100000;
+
 
 begin
+
+num_aux <=to_integer(unsigned(sw(11 downto 0)));
+num_aux1 <=to_integer(unsigned(sw(5 downto 0)));
+num_aux2 <=to_integer(unsigned(sw(11 downto 6)));
+                     
 
 process(clk, inicio)
 begin
 if inicio='1' then
-    cont<=0;
+    unidades<=0;
+    decenas<=0;
+    centenas<=0;
+    millares<=0;
 elsif rising_edge(clk) then
-    if cont=100000000 then
-        cont<=0;
-    else
-        cont<=cont+1;
+    if tipo = "00" then
+        unidades <= num_aux mod 10;
+        decenas <= (num_aux/10)mod 10;
+        centenas <= (num_aux/100)mod 10;
+        millares <= (num_aux/1000)mod 10;
+     elsif tipo = "01" then
+        unidades <= num_aux1 mod 10;
+        decenas <= (num_aux1/10)mod 10;
+        centenas <= num_aux2 mod 10;
+        millares <= (num_aux2/10)mod 10;
+     end if;
+end if;
+end process;                                              
+                        
+process(inicio, clk)
+begin
+if inicio='1' then
+   cont_base_enable<=0;
+elsif rising_edge(clk) then
+      if cont_base_enable=100000 then 
+         cont_base_enable<=0;
+      else
+         cont_base_enable<=cont_base_enable+1;
+      end if;
+end if;
+end process;
+
+process(clk,inicio)
+begin
+if inicio='1' then
+   enable_aux<="1110";
+elsif rising_edge(clk) then
+    if cont_base_enable=100000 then
+	      enable_aux<=enable_aux(2 downto 0)&enable_aux(3);
     end if;
 end if;
-end process;                        
+end process;
+--fin de la activación de los siete seg
 
-process(clk, inicio)
+enable_seg<=enable_aux;
+
+--multiplexado de las entradas al 7-seg
+process(enable_aux, unidades, decenas)
 begin
-if inicio='1' then
-    vector_aux<="00000000"&sw;
-    estado<="00";
-    cont_bits<= 0;
-    unidades<="0011";
-    decenas<="0011";
-    fin<='0';
-elsif rising_edge(clk) then
---if cont=0 then
-    case estado is
-    when "00" =>    cont_bits<=0;
-                    fin<='0';
-                    vector_aux<="00000000"&sw;
-                    if enable='1' then
-                        estado<="01";
-                    end if;
-    when "01" =>    cont_bits<=cont_bits+1;
-                    fin<='0';
-                    vector_aux<=vector_aux(10 downto 0)&'0';
-                    if cont_bits<3 then
-                        estado<="10";
-                    else
-                        estado<="11";
-                    end if;
-    when "10" =>    cont_bits<=cont_bits;
-                    fin<='0';
-                    if vector_aux(11 downto 8)>4 then
-                        vector_aux(11 downto 8)<=vector_aux(11 downto 8)+"0011";
-                    end if;
-                    if vector_aux(7 downto 4)>4 then
-                        vector_aux(7 downto 4)<=vector_aux(7 downto 4)+"0011";
-                    end if;
-                    estado<="01";
-    when "11" =>    cont_bits<=0;
-                    fin<='1';
-                    decenas<=vector_aux(11 downto 8);
-                    unidades<=vector_aux(7 downto 4);
-                    estado<="00";
-    when others =>  cont_bits<=0;
-                    fin<='0';
-                    estado<="00";
-    end case;
-end if;
---end if;
-end process;                                              
+case enable_aux is
+when "1110" => sal_mux<=unidades;
+when "1101" => sal_mux<=decenas;
+when "1011" => sal_mux<=centenas;
+when "0111" => sal_mux<=millares;
+when others => sal_mux<=0;
+end case;
+end process;
+
+--descripcion del decodificador BCD-7segmentos
+process(sal_mux)
+begin
+case sal_mux is
+when 0 => segmentos<="0000001";
+when 1 => segmentos<="1001111";
+when 2 => segmentos<="0010010";
+when 3 => segmentos<="0000110";
+when 4 => segmentos<="1001100";
+when 5 => segmentos<="0100100";
+when 6 => segmentos<="1100000";
+when 7 => segmentos<="0001111";
+when 8 => segmentos<="0000000";
+when 9 => segmentos<="0001100";
+when others => segmentos<="1111111";
+end case;
+end process;--final de decodificador BCD-7seg
 
 end Behavioral;
